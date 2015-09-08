83bcc824c018aa711554667e6ea442a5b710a70a
Scott <gleason.scott.a@gmail.com>
2015-09-08 22:52:31 +1000

    started version contorl on shell scripts. To be synced between uni and mac computers

---
+++ gitPullToMac.sh
@@ -0,0 +1,25 @@
+echo ‘Pulls GitHub directories’
+
+echo ‘Pulling LaTeX Thesis from GitHub’
+cd ~/Documents/GitHub/LaTeX_Thesis
+git pull origin master
+
+echo ‘Pulling Manufacture-Alloys from GitHub’
+cd ~/Documents/GitHub/Manufacture-Alloys
+git pull origin master
+
+echo ‘Pulling IPython-Notebooks from GitHub’
+cd ~/Documents/GitHub/IPython-Notebooks
+git pull origin master
+
+echo ‘Pulling Alloy-Casting-Notes from GitHub’
+cd ~/Documents/GitHub/Alloy-Casting-Notes
+git pull origin master
+
+echo ‘Pulling MsWord Thesis from GitHub’
+cd ~/Documents/GitHub/MsWord-PhD-Thesis
+git pull origin master
+
+echo ‘Pulling Ultrastable-Mg-Zn-Ca-BMGs from GitHub’
+cd ~/Documents/GitHub/Ultrastable-Mg-Zn-Ca-BMGs
+git pull origin master
