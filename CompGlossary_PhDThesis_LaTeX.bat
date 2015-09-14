b44720e4f22eb667573b27a42fdaa36a5679fa38
GleasonS3 <gleason.scott.a@gmail.com>
2015-09-14 11:02:33 +1000

    New Script - complies glossary for LaTeX thesis

---
+++ CompGlossary_PhDThesis_LaTeX.bat
@@ -0,0 +1,10 @@
+REM #Generates the glossary file the LaTeX PhD thesis. 
+REM #Must be added everytime a new glossary reference is created.
+
+cd C:\Users\z3492622\Documents\PhD Project\Documents\LaTeX_Thesis\
+
+makeindex -s "ScottGleason PhDThesis.ist" -o "ScottGleason PhDThesis.gls" "ScottGleason PhDThesis.glo"
+
+REM "LaTeX glossary generation complete"
+
+timeout 150
