ab59ced59eca00674b6058c0dec07d37d1e953f3
PoleScars <gleason.scott.a@gmail.com>
2016-02-23 15:08:59 +1100

    USB - EMU script now copies image drive to public drive

--- USB_Scripts/Transfer_EMU_MPub_To_USB.bat
+++ USB_Scripts/Transfer_EMU_MPub_To_USB.bat
@@ -7,14 +7,21 @@ REM "NOTE: Open Or Locked files will NOT copy!!!"
 
 
 
-REM "EMU s3400-I to EMU Public Drive Transfer"
-Robocopy.exe "M:\emuimages\s3400-I\z3492622" "M:\emupublic\z3492622-SG" /E
-REM "EMU s3400-I to EMU Public Drive Transfer complete"
+REM "EMU s3400-I & X to EMU Public Drive Transfer"
+Robocopy.exe "M:\emuimages\s3400-I\z3492622" "M:\emupublic\z3492622-SG\SEM" /E
+Robocopy.exe "M:\emuimages\s3400-X\z3492622_SG" "M:\emupublic\z3492622-SG\SEM" /E
+REM "EMU s3400-I & X to EMU Public Drive Transfer complete"
+
+REM "EMU AFM to EMU Public Drive Transfer"
+Robocopy.exe "M:\emuimages\afm\z3492622" "M:\emupublic\z3492622-SG\AFM\archive" /E
+REM "EMU AFM to EMU Public Drive Transfer complete"
+
 
 
 REM "EMU Public Drive to USB Transfer"
-Robocopy.exe "M:\emupublic\z3492622-SG\SEM" "F:\SEM" /E ^
-/XD "M:\emupublic\z3492622-SG\Old Data"
+Robocopy.exe "M:\emupublic\z3492622-SG\SEM" "F:\SEM" /E 
+Robocopy.exe "M:\emupublic\z3492622-SG\AFM" "F:\AFM" /E
+Robocopy.exe "M:\emupublic\z3492622-SG\FIB" "F:\FIB" /E
 
 REM "EMU Public Drive to USB Transfer complete"
 
