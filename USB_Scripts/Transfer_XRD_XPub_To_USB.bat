d668c33585796f415197749e206fcca40d9c6fdb
PoleScars <gleason.scott.a@gmail.com>
2016-02-26 08:33:38 +1100

    XRD to USB - corrected typos in the promp

--- USB_Scripts/Transfer_XRD_XPub_To_USB.bat
+++ USB_Scripts/Transfer_XRD_XPub_To_USB.bat
@@ -1,7 +1,7 @@
 REM #Formate of RoboCopy - robocopy "sourceDirectory" "destinationDirectory" /E
 
-REM "EMU - Coping ALL new directories & files from EMU Public Drive to USB Drive."
-REM "EMU - Overwriting ALL existing USB files whom's source timestamps have advanced."
+REM "XRD - Coping ALL new directories & files from XRD Public Drive to USB Drive."
+REM "XRD - Overwriting ALL existing USB files whom's source timestamps have advanced."
 
 REM "NOTE: Open Or Locked files will NOT copy!!!"
 
