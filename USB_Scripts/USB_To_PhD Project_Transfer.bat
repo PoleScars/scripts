582b2085537150d5ccd0af04b7bafabe52342d57
PoleScars <gleason.scott.a@gmail.com>
2016-04-22 16:24:53 +1000

    USB to PdD - Added Stylus Profiler to transfer script

--- USB_Scripts/USB_To_PhD Project_Transfer.bat
+++ USB_Scripts/USB_To_PhD Project_Transfer.bat
@@ -1,6 +1,6 @@
 REM #Formate of RoboCopy - robocopy "sourceDirectory" "destinationDirectory" /E
 
-REM "USB - Coping ALL new directories & files from to USB Drive."
+REM "USB - Coping ALL new directories & files from USB Drive."
 REM "USB - Overwriting ALL existing files whom's source timestamps have advanced."
 
 REM "NOTE: Open Or Locked files will NOT copy!!!"
@@ -8,10 +8,11 @@ REM "NOTE: Open Or Locked files will NOT copy!!!"
 
 
 REM "USB SEM, FIB, AFM, & XRD to PhD Project Transfer"
-Robocopy.exe "I:\SEM" "D:\Scott\Documents\PhD Project\Results\SEM" /E
-Robocopy.exe "I:\AFM" "D:\Scott\Documents\PhD Project\Results\AFM" /E
-Robocopy.exe "I:\FIB" "D:\Scott\Documents\PhD Project\Results\FIB" /E
-Robocopy.exe "I:\XRD" "D:\Scott\Documents\PhD Project\Results\XRD" /E
+Robocopy.exe "G:\SEM" "D:\Scott\Documents\PhD Project\Results\SEM" /E
+Robocopy.exe "G:\AFM" "D:\Scott\Documents\PhD Project\Results\AFM" /E
+Robocopy.exe "G:\FIB" "D:\Scott\Documents\PhD Project\Results\FIB" /E
+Robocopy.exe "G:\XRD" "D:\Scott\Documents\PhD Project\Results\XRD" /E
+Robocopy.exe "G:\Stylus Profiler" "D:\Scott\Documents\PhD Project\Results\Stylus Profiler" /E
 REM "USB SEM, FIB, AFM, & XRD to PhD Project Transfer complete"
 
 timeout 300
